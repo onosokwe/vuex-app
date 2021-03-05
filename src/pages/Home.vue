ca32190716eb8c6670d0daaff2555e4d930b308e
Andrew <57761772+onosokwe@users.noreply.github.com>
2021-03-05 07:09:26 +0100

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Hi {{ username }}</h1>
+    <h1>Hi {{ user }}</h1>
     <p>{{ secretMessage }}</p>
     <input type="button" value="Logout" @click="logout" />
   </div>
@@ -11,14 +11,14 @@ export default {
   data() {
     return {
       secretMessage: "",
-      username: "",
+      user: "",
     };
   },
   async created() {
     if (!this.$store.getters.isLoggedIn) {
       this.$router.push("/login");
     }
-    this.username = this.$store.getters.getUser.username;
+    this.user = this.$store.getters.getUser;
     this.secretMessage = await AuthService.getSecretContent();
   },
   methods: {
